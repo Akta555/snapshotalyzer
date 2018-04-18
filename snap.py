b051d1aec1debfdb3b4487c35e404e3ed3463469
Julien Fournier <jfournier2@free.fr>
2018-04-18 18:02:50 +0200

    ok

--- snap.py
+++ snap.py
@@ -2,9 +2,32 @@
 # s3 = boto3.resource('s3') 
 
 import boto3
+import click
 
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-for i in ec2.instances.all():
-    print(i)
+@click.command()
+@click.option('--project', default=None,
+    help="only instance for project (tag project:<name>)")
+def list_instances(project):
+    "List EC2 instances"
+    instances = []
+
+    if project:
+        #!tag name and value are case sensitive!
+        filters = [{'Name':'tag:project','Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    return
+
+list_instances()
