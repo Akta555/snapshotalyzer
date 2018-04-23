584a65c603675e724829b437116f878d0d257bcc
Julien Fournier <jfournier2@free.fr>
2018-04-23 15:36:10 +0200

    ok

--- snap.py
+++ snap.py
@@ -2,6 +2,7 @@
 # s3 = boto3.resource('s3') 
 
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -21,8 +22,92 @@ def filter_instances(project):
     return instances
 
 @click.group()
+def cli():
+    """Shotty manages snapshot"""
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="only snapshots for project (tag project:<name>)")
+def list_snapshots(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all(): 
+                print(', '.join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.start_time.strftime("%c")
+            )))
+
+    return
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="only volumes for project (tag project:<name>)")
+def list_volumes(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(', '.join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+
+
+# Instances group
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
+
+# Instances command snapshot
+
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--project', default=None,
+    help="only instances for project (tag project:<name>)")
+def create_snapshots(project):
+    "Create snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        i.stop()
+        i.wait_until_stopped()
+
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by snapscript") 
+
+        print("starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done")
+
+    return
+
+# Instances command list
 
 @instances.command('list')
 @click.option('--project', default=None,
@@ -45,6 +130,8 @@ def list_instances(project):
 
     return
 
+# Instances command stop
+
 @instances.command('stop')
 @click.option('--project', default=None,
     help='Only instances for project')
@@ -54,10 +141,16 @@ def stop_instance(project):
     instances = filter_instances(project)   
 
     for i in instances:
-        print("Stopping {0}...".format(i.id))
-        i.stop()
+        print("stopping {0}...".format(i.id))
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop {0}.".format(i.id) + str(e))
+            continue
 
     return
+
+# Instances command start
 
 @instances.command('start')
 @click.option('--project', default=None,
@@ -69,10 +162,13 @@ def start_instance(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
-
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}.".format(i.id) + str(e))
+            continue
     return
 
 
-
-instances()
+if __name__ == '__main__':
+    cli()
